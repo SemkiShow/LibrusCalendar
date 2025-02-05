90adf439c8e4315df84194eb6047684b1fea64ef
SemkiShow <mgdeveloper123@gmail.com>
2025-02-05 10:03:43 +0100

    Fixed the install script

--- install.sh
+++ install.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
-cp -r .. /usr/local/bin
-cd /usr/local/bin/LibrusCalendar
 nano librus_key.txt
 mkdir venv
 python3 -m venv venv/
+sudo cp -r .. /usr/local/bin
+cd /usr/local/bin/LibrusCalendar
 source venv/bin/activate
 pip install --upgrade google-api-python-client google-auth-httplib2 google-auth-oauthlib librus-apix
-cp librus.service /etc/systemd/system
-systemctl daemon-reload
-systemctl enable librus.service
-systemctl start librus.service
-sudo systemctl status librus.service
+sudo cp librus.service /etc/systemd/system
+sudo systemctl daemon-reload
+sudo systemctl enable librus.service
+sudo systemctl start librus.service
+systemctl status librus.service
