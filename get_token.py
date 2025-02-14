ae0c4e754d3f6ff574b0937fdb702544b49268b8
SemkiShow <mgdeveloper123@gmail.com>
2025-02-14 21:39:10 +0100

    Fixed the install script

--- get_token.py
+++ get_token.py
@@ -1,3 +1,4 @@
+import os, datetime
 from google.auth.transport.requests import Request
 from google.oauth2.credentials import Credentials
 from google_auth_oauthlib.flow import InstalledAppFlow
--- install.sh
+++ install.sh
@@ -5,7 +5,7 @@ python3 -m venv venv/
 source venv/bin/activate
 pip install --upgrade google-api-python-client google-auth-httplib2 google-auth-oauthlib librus-apix
 python3 get_token.py
-sudo cp -r .. /usr/local/bin
+sudo cp -r . /usr/local/bin/LibrusCalendar
 cd /usr/local/bin/LibrusCalendar
 source venv/bin/activate
 sudo cp librus.service /etc/systemd/system
